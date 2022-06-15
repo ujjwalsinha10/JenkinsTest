e87b1f55f65dd2d45f80e1530188ddb160770fee
ujjwalsinha10 <93342031+ujjwalsinha10@users.noreply.github.com>
2022-06-16 01:04:54 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld{
 	public static void main(String[] args){
 		System.out.println("First Program");
 		System.out.println("Made Some Changes");
-		System.out.println("**** Git Webhook Added$ ****");
+		System.out.println("**** $Git Webhook Added$ ****");
 	}
 }
